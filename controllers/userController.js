8d4641b0ef40c03b1aa5c001d7dbdd25869f5309
BuildTools <unconfigured@null.spigotmc.org>
2023-04-28 20:22:11 -0500

    Controller de user

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+import UserModel from '../models/UserModel.js'
+
+const createUser = async (req,res) =>{
+    try {
+        await UserModel.create(req.body)
+        res.status(201).json({
+            'message': 'Se ha creado el usuario corectamente'
+        })
+    } catch (error) {
+        res.status(404).json({
+            'message': 'Pailas, algo salio mal xdxd'
+    }
+}
